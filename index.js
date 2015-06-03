b3e58f9081da9702691515a78daa644183041f46
Mark K Cowan <mark@battlesnake.co.uk>
2015-06-03 20:59:04 +0300

    Removed requireDir for browserify compatibility

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var requireDir = require('require-dir');
-
 module.exports = {
-	parsers: requireDir('./parsers'),
-	languages: requireDir('./languages'),
-	util: requireDir('./util')
+	languages: {
+		arithmetic: require('./parsers/arithmetic'),
+		comprehension: require('./parsers/comprehension'),
+		list: require('./parsers/list')
+	},
+	parsers: {
+		arithmetic: require('./parsers/arithmetic'),
+		comprehension: require('./parsers/comprehension'),
+		list: require('./parsers/list'),
+		recursive: require('./parsers/recursive'),
+		simple: require('./parsers/simple')
+	},
+	util: {
+		languageBuilder: require('./util/language-builder')
+	}
 };
